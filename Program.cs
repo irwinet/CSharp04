bf41b0111252bbf19825a7fcf39fddd0c4d5c452
irwinet <irwinestradatorres@gmail.com>
2020-08-26 10:09:55 -0500

    Month Import

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pTotal = 0;
+
+            Console.WriteLine("Month: ");
+            string month = Console.ReadLine();
+
+            Console.WriteLine("Import: ");
+            float import = Convert.ToSingle(Console.ReadLine());
+
+            switch(month.ToUpper()){
+                case "OCTUBRE":
+                case "DICIEMBRE":
+                {
+                    pTotal = import - (import * 0.15f);
+                    break;
+                }
+                default:
+                {
+                    pTotal = import;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Price Total: {pTotal}");
+        }
+    }
+}
